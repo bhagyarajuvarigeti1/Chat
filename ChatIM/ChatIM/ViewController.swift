801f5909f9006219d6ac7eab70887bf71e7bfa05
Bhagyarajuv <bhagya.raju@myn.global>
2024-02-07 11:42:23 +0530

    initial commit

---
+++ ChatIM/ChatIM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatIM
+//
+//  Created by bhagyaraju on 07/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
